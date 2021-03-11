52138412b659d6ee44e1735e9cdeb49a5db9d70c
Liam Forsberg <fourzifer@gmail.com>
2021-03-11 16:27:11 +0100

    Updated the player prefabs to include animations and scripts

---
+++ Assets/Scripts/P2Controller.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P2Controller : MonoBehaviour
+{
+    float speed = 2f;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    Vector2 movement;
+
+    void Update()
+    {
+        //Player 1 Keyboard
+        movement.x = Input.GetAxis("P2_Horizontal");
+        movement.y = Input.GetAxis("P2_Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+
+        //animator.SetFloat("Horizontal", P1_horizontalInput);
+        //animator.SetFloat("Vertical", P1_verticalInput);
+
+        //if ((kbMovement.x < 0.01) || (kbMovement.x > 0.01))
+        //{
+
+        //}
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
